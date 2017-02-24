026e3bab4e849fc797f6ee869d05477b7bcbdd1a
Hal Owens <halowens@localhost.localdomain>
2017-02-23 20:53:26 -0700

    Update this to search until the regex works will add timing stuff

--- supreme.py
+++ supreme.py
@@ -23,22 +23,29 @@ def findItem(color, keywords):
     #Opens page to the hats --change to desired item
     supremeShop = br.open("http://www.supremenewyork.com/shop/all/" + data["subsection"])
     #Going to have to edit this to refresh the page unless it gets a result
-    htmlShop = supremeShop.read()
-    regex = re.compile(keywords+'.{0,100}'+color) #first find the objects which contain the adjectives
-    roughLink = re.findall(regex, htmlShop)
-    regexUrl = re.compile('\/shop\/[a-zA-Z0-9\/]*') #this will then find the link in the previously found strings
-    link = re.findall(regexUrl, roughLink[0])
+    link = [""]
+    while link[0] == "":
+        print "XD";
+        htmlShop = supremeShop.read()
+        regex = re.compile(keywords+'.{0,100}'+color) #first find the objects which contain the adjectives
+        roughLink = re.findall(regex, htmlShop)
+        regexUrl = re.compile('\/shop\/[a-zA-Z0-9\/]*') #this will then find the link in the previously found strings
+        print link
+        try:
+            link = re.findall(regexUrl, roughLink[0])
+            break
+        except IndexError:
+            link[0] = ""
     return 'http://www.supremenewyork.com' + link[0]
 
 
-browser.get(findItem("Blue", "Animal")) #Open the newly found link
-wait = WebDriverWait(browser, 10)
+browser.get(findItem(data["color"], data["keyword"])) #Open the newly found link
+wait = WebDriverWait(browser, 2)
 
 def pickSize(size): #Need to modify to actually select size might need to use regex
     if size != 'os':
         sizeList = browser.find_element_by_name("size")
-        sizeList.send_keys(Keys.ARROW_DOWN)
-
+        sizeList.send_keys(size)
 
 def addToCart():
     cart = browser.find_element_by_name('commit')  # Find the search box
@@ -48,12 +55,13 @@ def addToCart():
     checkout.click();
 
 def checkOut():
-    wait.until(EC.visibility_of_element_located((By.ID, "order_billing_name")))
+    wait.until(EC.visibility_of_element_located((By.ID, "order_billing_name"))) # need to add all of this stuff into a for loop to lazy to do it right now
+    #Temporary yet completely permanent patch together
     info = browser.find_element_by_id("order_billing_name")
     info.send_keys(data["name"])
     info = browser.find_element_by_id("order_email")
     info.send_keys(data["email"])
-    info = browser.find_element_by_id("order_tl")
+    info = browser.find_element_by_id("order_tel")
     info.send_keys(data["tel"])
     info = browser.find_element_by_id("bo")
     info.send_keys(data["address"])
@@ -78,87 +86,8 @@ def checkOut():
 
 
 
-pickSize("Large")
+
+pickSize(34)
 addToCart()
 checkOut()
-#for i in link:
-#    print i
-#link.send_keys(Keys.TAB)
-#link.send_keys(Keys.TAB) #To find the add to cart button which is initially hiden i go to the button before it in the tab order and tab over
-#link.send_keys(Keys.ENTER)
-
-#browser.quit()
-
-
-
-
-
-
-
-'''
-MECHANIZE
-
-Hal Owens
-Supreme Bot using Mechanize: http://wwwsearch.sourceforge.net/mechanize/
-Useful Links:
-http://www.pythonforbeginners.com/mechanize/browsing-in-python-with-mechanize/
-http://stockrt.github.io/p/emulating-a-browser-in-python-with-mechanize/
-
-
-https://pypi.python.org/pypi/selenium
-https://github.com/mozilla/geckodriver/releases
-
-    WHAT TO DO TO GET THE ITEM YOU WANT
-    Change value of supremeShop website variable to equal section of shop with item
-    change color/description in regex
-
-import mechanize
-import cookielib
-import re
-
-
-#open browser
-br = mechanize.Browser()
-
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
-
-# Browser options
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
-
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-
-# User-Agent
-# Makes robot look like a fedora machine running firefox to bypass bot protections
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-
-#Uncomment for debugging messages
-#br.set_debug_http(True)
-#br.set_debug_redirects(True)
-#br.set_debug_responses(True)
-
-
-#Opens page to the hats --change to desired item
-supremeShop = br.open("http://www.supremenewyork.com/shop/all/hats")
-#Going to have to edit this to refresh the page unless it gets a result
-htmlShop = supremeShop.read()
-regex = re.compile('Glossy.{0,100}Royal') #first find the objects which contain the adjectives
-roughLink = re.findall(regex, htmlShop)
-regexUrl = re.compile('\/shop\/[a-zA-Z0-9\/]*') #this will then find the link in the previously found strings
-link = re.findall(regexUrl, roughLink[0])
-productpage = br.open("http://www.supremenewyork.com" + link[0])
-print br.geturl()
-br.select_form(nr=0)
-br.submit()
-tempHtml = productpage.read()
-print tempHtml
-regexTemp = re.compile('class="logo".{0,500}') ##MAKE REGEX TO FIND OUT IF PRODUCT IS IN CART
-status = re.findall(regexTemp, tempHtml)
-print status
-'''
+print data[0]
